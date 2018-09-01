ca65ed0ea6ceafafc223a8765c9d3e91dec45662
Lena Zheng <lzhengem@gmail.com>
2018-08-31 18:51:18 -0700

    added menustate

--- core/src/com/lzhengem/game/FlappyDemo.java
+++ core/src/com/lzhengem/game/FlappyDemo.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.lzhengem.game.States.GameStateManager;
+import com.lzhengem.game.States.MenuState;
 
 public class FlappyDemo extends ApplicationAdapter {
     //width and height of our screen
@@ -12,27 +14,27 @@ public class FlappyDemo extends ApplicationAdapter {
 	public static final int HEIGHT = 800;
 
 	public static final String TITLE = "Flappy Bird";
-	SpriteBatch batch;
-	Texture img;
+	private GameStateManager gsm;
+	private SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
