66182c6843df6d8f6c095f588205852ac00b666f
Lena Zheng <lzhengem@gmail.com>
2018-08-31 18:27:37 -0700

    initial commit

---
+++ desktop/src/com/lzhengem/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lzhengem.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lzhengem.game.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}
