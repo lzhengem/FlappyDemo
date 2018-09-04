b2f406c6a3bf52310b38d8a391fc8811c896845a
Lena Zheng <lzhengem@gmail.com>
2018-09-04 16:28:26 -0700

    added bottom and top tubes

---
+++ core/src/com/lzhengem/game/sprites/Tube.java
@@ -0,0 +1,12 @@
+package com.lzhengem.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Tube {
+    private Texture tobTube, bottomTube;
+
+    public Tube(float x) {
+        tobTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+    }
+}
