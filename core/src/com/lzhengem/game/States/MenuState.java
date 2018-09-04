45707127c65606e82fc75468eba788aacce7a9a3
Lena Zheng <lzhengem@gmail.com>
2018-09-04 15:06:15 -0700

    added playbtn and bg

--- core/src/com/lzhengem/game/States/MenuState.java
+++ core/src/com/lzhengem/game/States/MenuState.java
@@ -1,11 +1,18 @@
 package com.lzhengem.game.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.lzhengem.game.FlappyDemo;
 
 public class MenuState extends State{
+    //set up the background and play button using the png from assess
+    private Texture background;
+    private Texture playBtn;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playBtn.png");
     }
 
     @Override
@@ -20,6 +27,12 @@ public class MenuState extends State{
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.begin();
+        //draw the background on the sprite
+        sb.draw(background,0,0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
+        //set the play button in the middle of the screen
+        sb.draw(playBtn,(FlappyDemo.WIDTH/2) - (playBtn.getWidth() / 2), FlappyDemo.HEIGHT/2);
+        sb.end();
 
     }
 }
