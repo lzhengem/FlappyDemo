c25aa26c849efef14bbd3e8b0508461b0c706f9a
Lena Zheng <lzhengem@gmail.com>
2018-09-04 23:49:24 -0700

    added flapping sound for bird

--- core/src/com/lzhengem/game/sprites/Bird.java
+++ core/src/com/lzhengem/game/sprites/Bird.java
@@ -1,5 +1,7 @@
 package com.lzhengem.game.sprites;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
@@ -17,6 +19,8 @@ public class Bird {
     private Rectangle bounds;
     private Animation birdAnimation;
 
+    private Sound flap;
+
     public Bird(int x, int y){
         position = new Vector3(x,y,0);
         velocity = new Vector3(0,0,0);
@@ -24,6 +28,7 @@ public class Bird {
         texture  = new Texture("birdanimation.png");
         birdAnimation = new Animation(new TextureRegion(texture),3,0.5f);
         bounds = new Rectangle(x,y,texture.getWidth()/3, texture.getHeight());
+        flap = Gdx.audio.newSound(Gdx.files.internal("sfx_wing.ogg"));
     }
 
     public void update(float dt){
@@ -55,7 +60,9 @@ public class Bird {
     }
 
     public void jump(){
+
         velocity.y = 250;
+        flap.play(0.5f);
     }
 
     public Rectangle getBounds(){
@@ -63,6 +70,8 @@ public class Bird {
     }
 
     public void dispose(){
+
         texture.dispose();
+        flap.dispose();
     }
 }
