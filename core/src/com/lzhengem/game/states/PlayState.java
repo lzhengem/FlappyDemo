05dda273df7fd5e6cf5863f448d987cd577ab1fe
Lena Zheng <lzhengem@gmail.com>
2018-09-04 18:38:58 -0700

    set ground

--- core/src/com/lzhengem/game/states/PlayState.java
+++ core/src/com/lzhengem/game/states/PlayState.java
@@ -3,6 +3,7 @@ package com.lzhengem.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.lzhengem.game.FlappyDemo;
 import com.lzhengem.game.sprites.Bird;
@@ -13,8 +14,12 @@ public class PlayState extends State {
     private static final int TUBE_SPACING = 125;
     //how many total tubes we will have
     private static final int TUBE_COUNT = 4;
+    //ground was too tall, so offset it further down the screen
+    private static final int GROUND_Y_OFFSET = -50;
     private Bird bird;
     private Texture bg;
+    private Texture ground;
+    private Vector2 groundPos1, groundPos2;
 
     private Array<Tube> tubes;
 
@@ -25,6 +30,9 @@ public class PlayState extends State {
         //view smaller portion of screen
         cam.setToOrtho(false, FlappyDemo.WIDTH/2,FlappyDemo.HEIGHT/2);
         bg = new Texture("bg.png");
+        ground = new Texture("ground.png");
+        groundPos1 = new Vector2((cam.position.x-cam.viewportWidth/2),GROUND_Y_OFFSET);
+        groundPos2 = new Vector2(((cam.position.x-cam.viewportWidth/2) + ground.getWidth()),GROUND_Y_OFFSET);
 
         //create all your tubes up to the max
         tubes = new Array<Tube>();
@@ -44,6 +52,7 @@ public class PlayState extends State {
     @Override
     public void update(float dt) {
         handleInput();
+        updateGround();
         bird.update(dt);
         //set the camera to follow the bird. move it 80 pixels to the right
         cam.position.x = bird.getPosition().x + 80;
@@ -54,13 +63,18 @@ public class PlayState extends State {
         for(int i = 0; i< tubes.size; i++){
             Tube tube = tubes.get(i);
             //if the tube is to the left of the screen, move it
-            if(cam.position.x - (cam.viewportWidth)/2 > tube.getPosTopTube().x + tube.getTopTube().getWidth()){
+            if(cam.position.x - (cam.viewportWidth/2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()){
                 tube.reposition(tube.getPosTopTube().x +((Tube.TUBE_WIDTH + TUBE_SPACING) *TUBE_COUNT));
             }
             //loops through to check if each tube hits the bird
             if (tube.collides(bird.getBounds())){
                 gsm.set(new PlayState(gsm));
             }
+        }
+
+        //if the bird hits the ground, reset it
+        if(bird.getPosition().y <= ground.getHeight() + GROUND_Y_OFFSET){
+            gsm.set(new PlayState(gsm));
         }
         cam.update(); //tells libgdx that the camera has been updated
 
@@ -81,6 +95,8 @@ public class PlayState extends State {
             sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
             sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         }
+        sb.draw(ground, groundPos1.x, groundPos1.y);
+        sb.draw(ground, groundPos2.x, groundPos2.y);
         sb.end();
 
     }
@@ -92,8 +108,18 @@ public class PlayState extends State {
         for(Tube tube: tubes){
             tube.dispose();
         }
+        ground.dispose();
         System.out.println("Play state disposed");
 
 
+    }
+
+    private void updateGround(){
+        if(cam.position.x - (cam.viewportWidth/2) > groundPos1.x + ground.getWidth() ){
+            groundPos1.add(ground.getWidth() * 2, 0);
+        }
+        if(cam.position.x - (cam.viewportWidth/2) > groundPos2.x + ground.getWidth() ){
+            groundPos2.add(ground.getWidth() * 2, 0);
+        }
     }
 }
